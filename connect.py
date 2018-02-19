0e1bf84931adcf2d4954f2b4307759b6cd531ee4
Chris Richards <crichards.j@gmail.com>
2018-02-19 21:59:57 +0000

    Fixed up parsing of email returned by fetch_email

--- connect.py
+++ connect.py
@@ -13,7 +13,21 @@ def openEmail():
   email = file.read()
   return email
 
-def connect_sirius():
+def select_form(form):
+  return form.attrs.get('id', None) == 'gform_7'
+
+def submitForm(br,email):
+  br.select_form(predicate=select_form)
+  br.form.set_all_readonly(False)
+  br.form['input_1'] = 'Edward'
+  br.form['input_3'] = 'Broxane'
+  br.form['input_14'] = email
+  br.form['input_15'] = email
+  br.form['input_9'] = 'M1S 2H5'
+  br.find_control("input_18.1").items[0].selected=True
+  br.submit(id='gform_submit_button_7')
+
+def connectSirius():
   br = mechanize.Browser()
   br.open("https://www.siriusxm.ca/freetrial/")
   forms = list(br.forms())
@@ -21,9 +35,50 @@ def connect_sirius():
     print f
 
   email = openEmail()
-  print email
+  submitForm(br,email)
 
-def connect_sharklasers():
+def parseEmailContent():
+  s = requests.session()
+  email = openEmail()
+  print email
+  m = re.match(r"^([^\@]+)",email)
+  user = m.group(1)
+ 
+  payload = {'f':'set_email_user', 'email_user': user}
+  req = s.post('http://api.guerrillamail.com/ajax.php', data=payload)
+  print req.text
+
+  payload = {'f':'check_email','seq':'0'}
+  req = s.get('http://api.guerrillamail.com/ajax.php', params=payload)
+  print req.text
+
+  #187028583
+  m = re.match(r"^.+\[\{\"mail_id\"\:\"([^\"]+).+\"mail_from\"\:\"([^\"]+)",req.text)
+  mail_id = m.group(1)
+  mail_from = m.group(2)
+
+  while True:
+    if "siriusxm.ca" not in mail_from:
+      time.sleep(20)
+      continue
+    else:
+      break
+  
+  payload = {'f':'fetch_email','email_id':mail_id}
+  req = s.get('http://api.guerrillamail.com/ajax.php', params=payload)
+  print req.text
+  
+  m = re.match(r".+Username\s+\\\/\s+Nom d'utilisateur\:\s+([^\r\n\s]+).+Password\s+\\\/\s+Mot de passe\:\s+([^\r\n\s]+)",req.text)
+  email = m.group(1)
+  password = m.group(2)
+
+  file = open('siriusEmail.txt','w')
+  file.write(email)
+  file.write("\n")
+  file.write(password)
+  file.close()
+  
+def createSharklasersEmail():
   s = requests.session()
   payload = {'f':'get_email_address'}
   req = s.get('http://api.guerrillamail.com/ajax.php', params=payload)
@@ -32,19 +87,6 @@ def connect_sharklasers():
   m = re.match(r"\{\"email_addr\"\:\"([^\@]+\@[^\"]+)",req.text)
   email = m.group(1)
   saveEmail(email)
-  print m.group(1)
 
-  #time.sleep(20)
-  payload = {'f':'check_email','seq':'0'}
-  req = s.get('http://api.guerrillamail.com/ajax.php', params=payload)
-  print req.text
 
-  try:
-      if req.text is None: # The variable
-          print('It is None')
-  except NameError:
-      print ("This variable is not defined")
-  else:
-      print ("It is defined and has a value")
-
-connect_sirius()
+parseEmailContent()
